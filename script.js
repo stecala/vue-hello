ff545278110db36af582f21d866bfd54f848bd60
stecala <ste.cala123@gmail.com>
2022-07-04 15:11:37 +0200

    estetical adjustments

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const myApp = new Vue ({
     el : '#root',
     data : {
         headContent : 'Titolo dinamico',
-        headClass : "text-center color",
-        imgSrc : 'https://media.wizards.com/2015/events/gpsao15/John-Avon-Unhinged.jpg',
+        headClass : "text-center color text-shadow",
+        imgSrc : 'https://c4.wallpaperflare.com/wallpaper/500/442/354/outrun-vaporwave-hd-wallpaper-thumb.jpg',
         
     },
     methods : {
